2f735b64a038620f9272e822fddc08120f02ec38
git <linzhijia0612@126.com>
2015-07-07 13:46:08 +0800

    add 789

--- DemoGit/src/com/test/TestGit.java
+++ DemoGit/src/com/test/TestGit.java
@@ -4,7 +4,7 @@ public class TestGit {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("git test 123456");
+		System.out.println("git test 123456789");
 		
 	}
 
