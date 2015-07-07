a9f7904ef6387e6e13e4381c90154956ba825eba
git <linzhijia0612@126.com>
2015-07-07 16:21:52 +0800

    add string test03

--- DemoGit/src/com/test/TestGit.java
+++ DemoGit/src/com/test/TestGit.java
@@ -6,6 +6,7 @@ public class TestGit {
 		// TODO Auto-generated method stub
 		String test = "123456789";
 		String test02 = "123456789";
+		String test03 = "123456789";
 		System.out.println("git test 123456789");
 		
 	}
